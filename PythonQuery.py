1edbb32eb1aac77f1b6e05c33a73f173d41886af
gpasxalis <p12gram@ionio.gr>
2017-10-23 21:07:38 +0300

    Query is done

--- PythonQuery.py
+++ PythonQuery.py
@@ -1,9 +1,26 @@
 from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
-res = es.search(index="test", body={"data": viaf}})
-res1 = es.search(index="test", body={"data": mitsos}})
+res = es.search(index="test", body={"query": {"match": {"data": "viaf"}}})
+res1 = es.search(index="test", body={"query": {"match": {"data": "mitsos"}}})
+res2 = es.search(index="test", body={"query": {"match": {"data": "GRANT DUFF"}}})
 print("Got %d Hits:" % res['hits']['total'])
+for hit in res['hits']['hits']:
+    print("%(data)s" % hit["_source"])
+#for doc in res['hits']['hits']:
+   # print("ID: %s, %s" % (doc['_id'], doc['_source']['data']))
+	
 print("Got %d Hits:" % res1['hits']['total'])
+for hit in res1['hits']['hits']:
+    print("%(data)s" % hit["_source"])
+#for doc in res1['hits']['hits']:
+   # print("ID: %s, %s" % (doc['_id'], doc['_source']['data']))
+
+print("Got %d Hits:" % res2['hits']['total'])
+for hit in res2['hits']['hits']:
+    print("%(data)s" % hit["_source"])
+#for doc in res2['hits']['hits']:
+    #print("ID: %s, %s" % (doc['_id'], doc['_source']['data']))
+
 
 
